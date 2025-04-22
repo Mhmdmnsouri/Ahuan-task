d7f629b1d356a146fa0e33fa740772f18772b84c
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-04-22 22:47:12 +0330

    Implement : Home page route

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/index.vue";
+import ProductsList from "@/views/ProductsList.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      name: "Products",
+      component: ProductsList,
     },
   ],
 });
