7fd491af49271eeb1139697047c3a7926f00fae2
Mhmdmnsouri <Bitshawcoder@gmail.com>
2025-04-22 22:44:17 +0330

    Implement : Search component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="form-control w-full max-w-xs mb-4">
+        <input type="text" class="input input-bordered w-full" v-model="searchTerm" @input="emitSearch"
+            placeholder="جستجو بر اساس عنوان..." />
+    </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+const searchTerm = ref('')
+const emit = defineEmits(['update:modelValue'])
+
+const emitSearch = () => {
+    emit('update:modelValue', searchTerm.value)
+}
+</script>
